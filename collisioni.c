d54a85d2479c7cec83ca685cccc9248d4eb33e70
Troddio <95751874+Troddio@users.noreply.github.com>
2022-01-04 00:19:48 +0100

    stampa e collisioni commentate

--- collisioni.c
+++ collisioni.c
@@ -1,42 +1,61 @@
+/**
+ * In questo file sorgente vengono definite le funzioni @collisione_missili_bombe, @collisione_bombe_navicella e
+ * @collisione_missili_nemici
+ */
 #include "collisioni.h"
-
+/**
+ * La funzione @collisione_missili_bombe gestisce la collisione tra i missili generati dalla navicella e le bombe
+ * generate dalle navicelle nemiche. I suoi paramentri in ingresso sono:
+ * @param missili - È un array di strutture @Oggetto che contiene tutti i dati di ogni singolo missile generato dalla
+ *                  navicella del giocatore.
+ * @param bombe_nem - È un array di strutture @Oggetto che contiene tutti i dati di ogni singola bomba generata dalle
+ *                    navi nemiche che si trovano i prima fila.
+ */
 void collisione_missili_bombe(Oggetto *missili, Oggetto *bombe_nem){
-    int i, j;
-    pthread_mutex_lock(&mtx_missili);
-    pthread_mutex_lock(&mtx_bombe);
+    int i, j; //indici cicli for
+    pthread_mutex_lock(&mtx_missili); ///blocca il mutex @mtx_missili
+    pthread_mutex_lock(&mtx_bombe); ///blocca il mutex @mtx_bombe
     /// collissione tra missili e bombe
+    /** Scorre tutto l'array dei missili e delle bombe ancora in vita**/
     for (i = 0; i < MAX_MISSILI; i++) {
         if (missili[i].thread_id != 0 && missili[i].vite != 0) {
             for (j = 0; j < M; j++) {
                 if (bombe_nem[j].thread_id != 0 && bombe_nem[j].vite != 0) {
+                    /** Se la posizione di un missile coincide con quella di una bomba nemica**/
                     if (missili[i].pos.x == bombe_nem[j].pos.x && missili[i].pos.y == bombe_nem[j].pos.y){
-                        //uccido il missile colliso
 
-                        pthread_cancel(missili[i].thread_id);
+                        pthread_cancel(missili[i].thread_id); //uccide il thread del missile colliso
                         missili[i].vite = 0;
-
-                        //uccido la bomba collisa
-                        pthread_cancel(bombe_nem[j].thread_id);
+                        pthread_cancel(bombe_nem[j].thread_id); //uccide il thread della bomba collisa
                         bombe_nem[j].vite = 0;
+                        /* Cancellazione del missile colliso dallo schermo*/
                         attron(COLOR_PAIR(0));
                         mvaddstr(missili[i].old_pos.y, missili[i].old_pos.x, " ");
                         mvaddstr(missili[i].pos.y, missili[i].pos.x, " ");
                         attroff(COLOR_PAIR(0));
-                        missili[i] = init;
+                        missili[i] = init; // la locazione del missile colliso viene rinizializzata con @init
                     }
                 }
             }
         }
     }
-    pthread_mutex_unlock(&mtx_missili);
-    pthread_mutex_unlock(&mtx_bombe);
+    pthread_mutex_unlock(&mtx_missili); ///sblocca il mutex @mtx_missili
+    pthread_mutex_unlock(&mtx_bombe); ///sblocca il mutex @mtx_bombe
 }
+/**
+ * La funzione @collisione_bombe_navicella gestisce la collisione tra la navicella e le bombe
+ * generate dalle navicelle nemiche. I suoi paramentri in ingresso sono:
+ * @param navicella - È una struttura @Oggetto che contiene tutti i dati relativi alla navicella del giocatore.
+ * @param bombe_nem - È un array di strutture @Oggetto che contiene tutti i dati di ogni singola bomba generata dalle
+ *                    navi nemiche che si trovano i prima fila.
+ */
 void collisione_bombe_navicella(Oggetto *navicella, Oggetto *bombe_nem){
-    int i,j,k;
-    pthread_mutex_lock(&mtx_nave);
-    pthread_mutex_lock(&mtx_bombe);
+    int i,j,k; // indici cicli for
+    pthread_mutex_lock(&mtx_nave); ///blocca il mutex @mtx_nave
+    pthread_mutex_lock(&mtx_bombe); ///blocca il mutex @mtx_bombe
 
     ///collisione tra bombe e navicella player
+    /** Scorre tutto l'array delle bombe e verifica che le bombe siano ancora in vita **/
     for (i = 0; i < M; i++) {
         if (bombe_nem[i].thread_id != 0) {
             for (j = 0; j < DIM_NAVICELLA; ++j) {
@@ -44,11 +63,10 @@ void collisione_bombe_navicella(Oggetto *navicella, Oggetto *bombe_nem){
                     if (j > 0 && j<5){
                         k = 5;
                     }
+                    /* Se la posizione di una bomba coincide con quella della navicella del giocatore*/
                     if(bombe_nem[i].pos.x == navicella->pos.x + k && bombe_nem[i].pos.y == navicella->pos.y + j){
-//                          uccido la bomba collisa
-                        pthread_cancel(bombe_nem[i].thread_id);
-                        printf("\a");
-                        fflush(stdout);
+                        pthread_cancel(bombe_nem[i].thread_id); //Uccide il thread della bomba collisa
+                        beep(); // Genera un effetto sonoro che indica la collisione
                         bombe_nem[i].vite = 0;
                         navicella->vite--;
                     }
@@ -56,36 +74,48 @@ void collisione_bombe_navicella(Oggetto *navicella, Oggetto *bombe_nem){
             }
         }
     }
-    pthread_mutex_unlock(&mtx_nave);
-    pthread_mutex_unlock(&mtx_bombe);
+    pthread_mutex_unlock(&mtx_nave); ///sblocca il mutex @mtx_nave
+    pthread_mutex_unlock(&mtx_bombe); ///sblocca il mutex @mtx_bombe
 }
+/**
+ * La funzione @collisione_missili_bombe gestisce la collisione tra i missili generati dalla navicella e le bombe
+ * generate dalle navicelle nemiche. I suoi paramentri in ingresso sono:
+ * @param missili - È un array di strutture @Oggetto che contiene tutti i dati di ogni singolo missile generato dalla
+ *                  navicella del giocatore.
+ * @param enemies - È un array di strutture @Oggetto che contiene tutti i dati di ogni singola navicella nemica.
+ * @param primafila - È l'indirizzo di una variabile che contiene un @_Bool
+ */
 void collisione_missili_nemici(Oggetto *enemies, Oggetto *missili, _Bool *primafila) {
-    pthread_mutex_lock(&mtx_nemici);
-    pthread_mutex_lock(&mtx_missili);
-    int i, j, k, l;
+    pthread_mutex_lock(&mtx_nemici); ///blocca il mutex @mtx_nemici
+    pthread_mutex_lock(&mtx_missili); ///blocca il mutex @mtx_missili
+    int i, j, k, l; // indici cicli for
     ///collisione tra Missili e Nemici
+    /** Scorre tutto l'array dei nemici e dei missili ancora in vita**/
     for (i = 0; i < M; i++) {
         if (enemies[i].vite != 0) {
             for (j = 0; j < MAX_MISSILI; j++) {
                 if (missili[j].vite != 0) {
                     for (k = 0; k < DIM_NEMICO; ++k) {
                         for (l = 0; l < DIM_NEMICO; ++l) {
+                            /** Se la posizione di un missile coincide con quella di una navicella nemica**/
                             if (missili[j].pos.x == enemies[i].pos.x + l && missili[j].pos.y == enemies[i].pos.y + k) {
-                                pthread_cancel(missili[j].thread_id);
+                                pthread_cancel(missili[j].thread_id); // uccide il thread del missile colliso
                                 missili[j].vite = 0;
-                                enemies[i].vite -= 1;
-
+                                enemies[i].vite -= 1; // decrementa le vite della navicella nemica
+                                /** Se la navicella nemica ha perso tutte le vite **/
                                 if ((i - num_righe) >= 0 && enemies[i].vite == 0){
-                                    primafila[i-num_righe] = TRUE;
+                                    primafila[i-num_righe] = TRUE; // consente alla navicella nemica che la segue a sparare le bombe**/
                                 }
+                                /* Cacellazione del missile colliso */
                                 attron(COLOR_PAIR(0));
                                 mvaddstr(missili[j].old_pos.y, missili[j].old_pos.x, " ");
                                 attroff(COLOR_PAIR(0));
-                                missili[j] = init;
+                                missili[j] = init; // la locazione del missile colliso viene rinizializzato con @init
+                                /* Se la navicella nemica colpita ha terminato le vite */
                                 if (enemies[i].vite == 0){
-                                    pthread_cancel(enemies[i].thread_id);
-                                    stampaNemico(&enemies[i]); //cancello il nemico morto
-                                    enemies[i] = init;
+                                    pthread_cancel(enemies[i].thread_id); // uccide il thread della navicella nemica
+                                    stampaNemico(&enemies[i]); //cancello la navicella nemica distrutta
+                                    enemies[i] = init; // la locazione della navicella ditrutta viene rinizializzata con @init
                                 }
                             }
                         }
@@ -94,6 +124,6 @@ void collisione_missili_nemici(Oggetto *enemies, Oggetto *missili, _Bool *primaf
             }
         }
     }
-    pthread_mutex_unlock(&mtx_nemici);
-    pthread_mutex_unlock(&mtx_missili);
+    pthread_mutex_unlock(&mtx_nemici); ///sblocca il mutex @mtx_nemici
+    pthread_mutex_unlock(&mtx_missili); ///sblocca il mutex @mtx_missili
 }
--- stampa.c
+++ stampa.c
@@ -1,31 +1,36 @@
 #include "stampa.h"
-
+/// Inizializzazione grafica della navicella del giocatore
  char *nave[DIM_NAVICELLA]= {" ▟█▛▀▀",
                              "▟██▙  ",
                              "▗▟█▒▙▖",
                              "▝▜█▒▛▘",
                              "▜██▛  ",
                              " ▜█▙▄▄"};
-
+/// Inizializzazione grafica della navicella nemica di livello 1
  char *nemico_lv1[DIM_NEMICO]={"▀█▙",
                                "▒█ ",
                                "▄█▛"};
-
+/// Inizializzazione grafica della navicella nemica di livello 2
  char *nemico_lv2[DIM_NEMICO]={" △ ",
                                "◁ ◊",
                                " ▽ "};
+/// Inizializzazione grafica della navicella nemica di livello 3
  char *nemico_lv3[DIM_NEMICO]= {" ☠ ",
                                 "☠ ☠",
                                 " ☠ "};
-
+/**
+ *  La funzione @stampaNavicella stampa la navicella del giocatore nella posizione corrente e cancella dallo schermo
+ *  la stampa relativa alla posizione precedente in cui si trovava la navicella. Il suo parametro in ingresso è:
+ * @param navicella - È una struttura @Oggetto che contiene tutti i dati relativi alla navicella del giocatore.
+ */
 void stampaNavicella(Oggetto *navicella) {
-    int i;
+    int i; // indice ciclo for
 
-    pthread_mutex_lock(&mtx_nave);
-    Oggetto temp_navicella = *navicella;
-    navicella->old_pos = navicella->pos;
-    pthread_mutex_unlock(&mtx_nave);
-    /* cancello la 'vecchia' posizione della navicella */
+    pthread_mutex_lock(&mtx_nave); ///blocca il mutex @mtx_nave
+    Oggetto temp_navicella = *navicella; /// variabile temporanea per evitare di invocare ripetutamente il mutex @mtx_nave
+    navicella->old_pos = navicella->pos; // salva la vecchia posizione della navicella del giocatore
+    pthread_mutex_unlock(&mtx_nave); ///sblocca il mutex @mtx_nave
+    /* Cancella la 'vecchia' posizione della navicella */
     if (temp_navicella.old_pos.x >= 0) {
         attron(COLOR_PAIR(0));
         for (i = 0; i < DIM_NAVICELLA; i++) {
@@ -34,25 +39,32 @@ void stampaNavicella(Oggetto *navicella) {
         attroff(COLOR_PAIR(0));
     }
     attron(COLOR_PAIR(2));
+    /* Stampa la 'nuova' posizione della navicella*/
     for (i = 0; i < DIM_NAVICELLA; i++) {
         if (i <= 1 || i >= 4) {
+            // Colora le 'parti esterne' della navicella
             attron(COLOR_PAIR(6));
             mvaddstr(temp_navicella.pos.y + i, temp_navicella.pos.x, nave[i]);
             attroff(COLOR_PAIR(6));
         } else {
+            // Colora la 'parte centrale' della navicella
             attron(COLOR_PAIR(2));
             mvaddstr(temp_navicella.pos.y + i, temp_navicella.pos.x, nave[i]);
             attroff(COLOR_PAIR(2));
         }
     }
 }
-
+/**
+ *  La funzione @stampaNemico stampa le navicelle nemiche nella posizione corrente e cancella dallo schermo
+ *  la stampa relativa alla posizione in cui si trovavano precedentemente. Il suo parametro in ingresso è:
+ * @param enemy - È una struttura @Oggetto che contiene tutti i dati relativi alla singola navicella nemica.
+ */
 void stampaNemico(Oggetto *enemy){
-    int i;
-    Oggetto enemy_temp;
-    enemy_temp = *enemy;
+    int i; // indice ciclo for
+    Oggetto enemy_temp; // variabile temporanea per salvare i dati della navicella da stampare
+    enemy_temp = *enemy; //inzializza la variabile temporanea
 
-    /*Controlla che il nemico sia stato appena inizializzato*/
+    /** Se la navicella nemica è stata appena inizializzata **/
     if (enemy_temp.old_pos.y != -1 && enemy_temp.old_pos.x != -1) {
         attron(COLOR_PAIR(0));
         for (i = 0; i < DIM_NEMICO; i++) {
@@ -60,9 +72,9 @@ void stampaNemico(Oggetto *enemy){
         }
         attroff(COLOR_PAIR(0));
     }
-    /*Controlla quante vite ha il nemico con relativa stampa*/
+    /** Controlla quante vite ha il nemico con relativa stampa **/
     switch (enemy_temp.vite) {
-        //Stampa nemico livello 1
+        // Se ha tre vite stampa nemico livello 1
         case 3:
             for (i = 0; i < DIM_NEMICO; i++) {
                 if (i == 1) {
@@ -76,7 +88,7 @@ void stampaNemico(Oggetto *enemy){
                 }
             }
             break;
-            //Stampa nemico livello 2
+            // Se ha due vite stampa nemico livello 2
         case 2:
             attron(COLOR_PAIR(5));
             for (i = 0; i < DIM_NEMICO; i++) {
@@ -84,7 +96,7 @@ void stampaNemico(Oggetto *enemy){
             }
             attroff(COLOR_PAIR(1));
             break;
-            //Stampa nemico livello 3
+            // Se ha una vita stampa nemico livello 3
         case 1:
             attron(COLOR_PAIR(5));
             for (i = 0; i < DIM_NEMICO; i++) {
@@ -92,7 +104,7 @@ void stampaNemico(Oggetto *enemy){
             }
             attroff(COLOR_PAIR(1));
             break;
-            //Cancella la navicella nemica perchè ha zero vite
+            // Se ha zero vite cancella la navicella nemica
         case 0:
             attron(COLOR_PAIR(0));
             for (i = 0; i < DIM_NEMICO; i++) {
@@ -102,19 +114,31 @@ void stampaNemico(Oggetto *enemy){
             break;
     }
 }
-
+/**
+ *  La funzione @stampaBomba stampa le bombe nemiche nella posizione corrente e cancella dallo schermo
+ *  la stampa relativa alla posizione in cui si trovavano precedentemente. Il suo parametro in ingresso è:
+ * @param bomba - È una struttura @Oggetto che contiene tutti i dati relativi alla singola bomba nemica.
+ */
 void stampaBomba(Oggetto *bomba) {
+    // Se la bomba è all'interno dalla finestra di gioco
     if (bomba->pos.x != -1 && bomba->pos.y != -1) {
         attron(COLOR_PAIR(0));
-        mvprintw(bomba->old_pos.y, bomba->old_pos.x, " ");
+        mvprintw(bomba->old_pos.y, bomba->old_pos.x, " "); // cancella la stampa dell'ultima posizione della bomba
         attroff(COLOR_PAIR(0));
     }
+    /* Stampa la bomba nella posizione corrente*/
     attron(COLOR_PAIR(7));
-    mvprintw(bomba->pos.y, bomba->pos.x, "<"); ///♿ ⟢ ⁂ ꗇ ꗈ 💣 🚀 卐 ◌́ ◌͂ ✝
+    mvprintw(bomba->pos.y, bomba->pos.x, "⁂"); ///♿ ⟢ ⁂ ꗇ ꗈ 💣 🚀 卐 ◌́ ◌͂ ✝
     attroff(COLOR_PAIR(7));
 }
-
+/**
+ * La funzione @stampaMissile stampa i missili nella posizione corrente e cancella dallo schermo
+ *  la stampa relativa alla posizione in cui si trovavano precedentemente. Il suo parametro in ingresso è:
+ * @param missile - È una struttura @Oggetto che contiene tutti i dati del singolo missile generato dalla
+ *                  navicella del giocatore.
+ */
 void stampaMissile(Oggetto *missile) {
+    /* Se il missile è all'interno dalla finestra di gioco*/
     if (missile->pos.x != -1 && missile->pos.y != -1) {
         attron(COLOR_PAIR(0));
         mvprintw(missile->old_pos.y, missile->old_pos.x, " ");
